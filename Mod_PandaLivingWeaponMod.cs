67d5a5159b9d2e5fb2a8e136b6d34fe0e50dc529
jason0320 <jasonnosaj0320@gmail.com>
2025-03-25 22:00:44 +0800

    Whitelisted absorb enchants

--- Mod_PandaLivingWeaponMod.cs
+++ Mod_PandaLivingWeaponMod.cs
@@ -13,12 +13,14 @@ namespace PandaLivingWeaponMod
     public class Mod_PandaLivingWeaponMod : BaseUnityPlugin
     {
         public static ConfigEntry<bool> allowInvokes;
+        public static ConfigEntry<bool> allowAbsorbs;
         public static ConfigEntry<bool> globalExp;
         public static ConfigEntry<int> maxEnchRoll;
         public static ConfigEntry<int> livingGenRarity;
         private void Start()
         {
             allowInvokes = Config.Bind("General", "allowInvokes", false, "Allow spell trigger enchants spawn in selector");
+            allowAbsorbs = Config.Bind("General", "allowAbsorbs", false, "Allow absorb life/mana/stamina enchants spawn in selector");
             globalExp = Config.Bind("General", "globalExp", false, "Allow all equipped living items gain exp from enemy death");
             maxEnchRoll = Config.Bind("General", "maxEnchRoll", 10, "Set the maximum number of enchants in selector");
             livingGenRarity = Config.Bind("General", "livingGenRarity", 20, "Set the rarity of living weapon, 1 in x chance");
